0c95ef77e15583e52e223526daa5f0fbef1f7526
stoilov <ivan.bsto@gmail.com>
2015-12-13 14:16:15 +0200

    Add Stack and Queue

---
+++ DSA/src/linearStructures/Queue.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linearStructures;
+
+/**
+ *
+ * @author stoilov
+ */
+public class Queue<T> {
+
+    private int front;
+    private int rear;
+    private int length;
+    private T[] queue;
+    private boolean empty;
+
+    public Queue() {
+        length = 2;
+        queue = (T[]) new Object[length];
+        front = rear = 0;
+        empty = true;
+    }
+
+    public void enqueue(T element) {
+        queue[rear++] = element;
+        empty = false;
+
+        if (rear == length) {
+            rear = 0;
+        }
+
+        if (rear == front) {
+            length *= 2;
+            copyQueue();
+        }
+    }
+
+    public T dequeue() {
+        if (empty) {
+            return null;
+        }
+
+        T dequeued = queue[front];
+        queue[front] = null;
+        front++;
+        
+        if (front == length) {
+            front = 0;
+        }
+
+        if (front == rear) {
+            empty = true;
+        }
+
+        if (rear - front > 0 && (rear - front) * 4 == length) {
+            length /= 2;
+            T[] newQueue = (T[]) new Object[length];
+
+            int filled = 0;
+            for (int i = front; i < rear; i++) {
+                newQueue[filled] = queue[i];
+                filled++;
+            }
+
+            front = 0;
+            rear = filled;
+            queue = newQueue;
+        }
+
+        if (front - rear > 0 && (length - front + rear) * 4 == length) {
+            length /= 2;
+            copyQueue();
+        }
+
+        return dequeued;
+    }
+    
+    private void copyQueue() {
+        T[] newQueue = (T[]) new Object[length];
+
+            int filled = 0;
+            for (int i = front; i < queue.length; i++) {
+                newQueue[filled] = queue[i];
+                filled++;
+            }
+
+            for (int i = 0; i < rear; i++) {
+                newQueue[filled] = queue[i];
+                filled++;
+            }
+
+            front = 0;
+            rear = filled;
+            queue = newQueue;
+    }
+    
+    public boolean isEmpty() {
+        return empty;
+    }
+
+}
---
+++ DSA/src/linearStructures/Stack.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linearStructures;
+
+/**
+ *
+ * @author stoilov
+ * @param <T>
+ */
+public class Stack<T> {
+
+    private int length;
+    T[] stack;
+    private int current;
+
+    public Stack() {
+        current = 0;
+        length = 2;
+        stack = (T[]) new Object[length];
+    }
+
+    public void push(T element) {
+        if (element == null) {
+            return;
+        }
+
+        if (current == length - 1) {
+            length *= 2;
+
+            T[] newStack = (T[]) new Object[length];
+
+            for (int i = 0; i < stack.length; i++) {
+                newStack[i] = stack[i];
+            }
+
+            stack = newStack;
+        }
+
+        stack[current++] = element;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        T popped = stack[--current];
+        stack[current] = null;
+
+        if (length == (current + 1) * 4) {
+            length /= 2;
+            T[] newStack = (T[]) new Object[length];
+
+            for (int i = 0; i < length; i++) {
+                newStack[i] = stack[i];
+            }
+
+            stack = newStack;
+        }
+
+        return popped;
+    }
+
+    public boolean isEmpty() {
+        return stack[0] == null;
+    }
+
+}
